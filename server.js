e16ec518bb04f4a24a816afe825ae343ca35866e
Diego Caceres <caceresdiegoleonel@gmail.com>
2026-08-11 12:40:32 -0300

    "se realiza ajusten para Railway"

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import app from './Desarrollo/app.js';
 const PORT = Number(process.env.PORT) || 3000;
 const HOST = '0.0.0.0';
 
-// Inicia el servidor Express en el puerto definido
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Servidor corriendo en ${HOST}:${PORT}`);
 });
